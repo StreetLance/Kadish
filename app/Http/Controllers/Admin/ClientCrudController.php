950b5e169434e535fa52003a76ad6a9b03afe565
Krash <abezkrovnyi@mail.ru>
2019-09-29 21:44:29 +0300

    commit-admin_panel+plaques+fix_bug

---
+++ app/Http/Controllers/Admin/ClientCrudController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\ClientRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class ClientCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read CrudPanel $crud
+ */
+class ClientCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    public function setup()
+    {
+        $this->crud->setModel('App\Models\Client');
+        $this->crud->setRoute(config('backpack.base.route_prefix') . '/Client');
+        $this->crud->setEntityNameStrings('client', 'clients');
+    }
+
+    protected function setupListOperation()
+    {
+        // TODO: remove setFromDb() and manually define Columns, maybe Filters
+//        $this->crud->setFromDb();
+        $this->crud->setColumns([
+            'id',
+            'Name',
+            'Last_Name',
+            'Email',
+            'Phone_number',
+            'Social_relations']);
+    }
+
+    protected function setupCreateOperation()
+    {
+        $this->crud->setValidation(ClientRequest::class);
+
+        // TODO: remove setFromDb() and manually define Fields
+        $this->crud->setFromDb();
+    }
+
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+}
