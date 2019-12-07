5b25319e3970d0b06a4d252a492d5196f7e2b664
Krash <abezkrovnyi@mail.ru>
2019-12-07 23:53:18 +0200

    rework emails

--- resources/views/mail/Rminder_Seven_Dey.blade.php
+++ resources/views/mail/Rminder_Seven_Dey.blade.php
@@ -16,49 +16,48 @@
                 <tbody>
                 <tr>
                     <td align="center" style="padding-bottom:10px;">
-                        <img src="http://kaddish-prayer.com/kpw/img/kaddish-prayer_email.gif" alt="Kaddish Prayer"
+                        <img src="{{asset("images/mail/kaddish-prayer_email.gif")}}" alt="Kaddish Prayer"
                              width="48" height="60" border="0" style="display:block;"/>
                     </td>
                 </tr>
                 <tr>
-                    <td align="center" width="100%"
-                        style="border:1px solid #c6c6c6;padding:1px;background-color:#ffffff;width:100%"
-                        bgcolor="#ffffff">
+                    {{--                    <td align="center" width="100%"--}}
+                    {{--                        style="border:1px solid #c6c6c6;padding:1px;background-color:#ffffff;width:100%"--}}
+                    {{--                        bgcolor="#ffffff">--}}
 
-                    </td>
-                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tbody>
-                        <tr><td style="padding:20px;font-size:14px;color:#000;text-align:center;">
-{{--	<span style="font-size:18px;color:#000;">--}}
-{{--		Здравствуйте, Алексей!<br /><br />--}}
-{{--		Через 7 дней состоится йорцайт<br />--}}
-{{--		вашего дедушки Авраам бен Борух<br /><br />--}}
-{{--	</span>--}}
-                            </td></tr>
-                        </tbody></table>
-                    <table border="0" cellpadding="0" cellspacing="0" width="" style="background-color:#ffcc66" bgcolor="#ffcc66"><tbody>
-                        <tr><td style="padding:16px;text-align:center;">
-                                <a href="" style="text-decoration:none;color:#000000;font: 18px Arial, sans-serif;line-height:22px;-webkit-text-size-adjust:none;" target="_blank">Заказать чтение молитвы Кадиш</a>
-                            </td></tr></tbody></table>
+                    {{--                    </td>--}}
+                    {{--                    <table border="0" cellpadding="0" cellspacing="0" width="100%" style="width:100%;"><tbody>--}}
+                    {{--                        <tr><td style="padding:20px;font-size:14px;color:#000;text-align:center;">--}}
+                    {{--	<span style="font-size:18px;color:#000;">--}}
+                    {{--		Здравствуйте, Алексей!<br /><br />--}}
+                    {{--		Через 1 дней состоится йорцайт<br />--}}
+                    {{--		вашего дедушки Авраам бен Борух<br /><br />--}}
+                    {{--	</span>--}}
+                    {{--                            </td></tr>--}}
+                    {{--                        </tbody></table>--}}
+                    {{--                    <table border="0" cellpadding="0" cellspacing="0" width="" style="background-color:#ffcc66" bgcolor="#ffcc66"><tbody>--}}
+                    {{--                        <tr><td style="padding:16px;text-align:center;">--}}
+                    {{--                                <a href="" style="text-decoration:none;color:#000000;font: 18px Arial, sans-serif;line-height:22px;-webkit-text-size-adjust:none;" target="_blank">Заказать чтение молитвы Кадиш</a>--}}
+                    {{--                            </td></tr></tbody></table>--}}
                     <br /><br />
                     <table border="0" cellpadding="0" cellspacing="0" width="100%" style="width:100%;background-color:#f3f2ef" bgcolor="#f3f2ef"><tbody>
                         <tr><td style="padding:16px;font-size:14px;color:#000;">
-                                The yorzait of your dear ones will be happening on oct 20th night, it is a special time for the soul of your beloved. The Shabbos prior the elevation of the soul begins .</br>
-                                The customs observed</br>
-                                *The Shabos prior to Yorzait go to your local synagogue to say kadish(there are 3 prayers trough Shabbos : evening ,morning  & afternoon).</br>
-                                *Sponsor a kidush (Dinner ,lunch or a sprits bottle)in honor of the elevation of the soul.</br>
-                                *the actual date of Yorzait gather a minyan(quorum of 10 Jewish men) at the cemetery or synagogue (Kadish should be said in all 3 prayers)</br>
-                                *light a 24/hr candle at the grave or home (home in the evening when Yorzait starts).</br>
-                                * add in charity and Mitzvos/good deeds on the actual date.</br>
-                                С уважением и наилучшими пожеланиями,
-                                <table border="0" cellpadding="0" cellspacing="0" width=""><tbody>
-                                    <tr><td width="40px" style="width:40px;">
-                                            <img src="http://kaddish-prayer.com/kpw/img/kaddish-prayer_bl.gif" width="40" height="40" border="0" style="display:block" />
-                                        </td>
-                                        <td style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:10px;font-size:14px;">
-                                            Р. Шмуэль Лившиц<br />
-                                            Руководитель проекта
-                                        </td></tr>
-                                    </tbody></table>
+                                The yorzait of your dear ones will be happening on oct 20th night, it is a special time for the soul of your beloved. The Shabbos prior the elevation of the soul begins .<br>
+                                The customs observed <br>
+                                *The Shabos prior to Yorzait go to your local synagogue to say kadish(there are 3 prayers trough Shabbos : evening ,morning  & afternoon).<br>
+                                *Sponsor a kidush (Dinner ,lunch or a sprits bottle)in honor of the elevation of the soul.<br>
+                                *the actual date of Yorzait gather a minyan(quorum of 10 Jewish men) at the cemetery or synagogue (Kadish should be said in all 3 prayers)<br>
+                                *light a 24/hr candle at the grave or home (home in the evening when Yorzait starts).<br>
+                                * add in charity and Mitzvos/good deeds on the actual date.<br>
+                                {{--                                <table border="0" cellpadding="0" cellspacing="0" width=""><tbody>--}}
+                                {{--                                    <tr><td width="40px" style="width:40px;">--}}
+                                {{--                                            <img src="http://kaddish-prayer.com/kpw/img/kaddish-prayer_bl.gif" width="40" height="40" border="0" style="display:block" />--}}
+                                {{--                                        </td>--}}
+                                {{--                                        <td style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:10px;font-size:14px;">--}}
+                                {{--                                            Р. Шмуэль Лившиц<br />--}}
+                                {{--                                            Руководитель проекта--}}
+                                {{--                                        </td></tr>--}}
+                                {{--                                    </tbody></table>--}}
                             </td></tr>
                         </tbody></table>
                 </tr>
@@ -68,22 +67,22 @@
                             <tbody>
                             <tr>
                                 <td width="40px" style="width:40px;padding-top:10px;">
-                                    <img src="http://kaddish-prayer.com/kpw/img/kaddish-prayer_email_footer.gif"
+                                    <img src="{{asset("images/mail/kaddish-prayer_email_footer.gif")}}"
                                          width="32" height="40" border="0" style="display:block"/>
                                 </td>
                                 <td valign="middle"
                                     style="padding-top:10px;padding-right:0;padding-bottom:0;padding-left:8px;font-size:12px;width:80%;color:#777777;vertical-align:middle;line-height:20px;">
                                     <b>Kaddish Prayer</b>: Remind Yortzait and Arrange Kaddish<br/>
-                                    <a href="qwe"
+                                    <a href="http://149.28.51.36/en"
                                        style="padding-right:4px;color:#194678;font: 12px Arial, sans-serif;line-height:20px;-webkit-text-size-adjust:none;text-decoration:underline;"
                                        target="_blank">Arrange Kaddish</a>
-                                    <a href="qwe"
+                                    <a href="http://149.28.51.36/en/wiki"
                                        style="padding-right:4px;color:#194678;font: 12px Arial, sans-serif;line-height:20px;-webkit-text-size-adjust:none;text-decoration:underline;"
                                        target="_blank">What is kaddish</a>
-                                    <a href="qwe"
+                                    <a href="http://149.28.51.36/en/wiki"
                                        style="padding-right:4px;color:#194678;font: 12px Arial, sans-serif;line-height:20px;-webkit-text-size-adjust:none;text-decoration:underline;"
                                        target="_blank">What is Yortzait</a>
-                                    <a href="qwe"
+                                    <a href="http://149.28.51.36/en/about"
                                        style="padding-right:4px;color:#194678;font: 12px Arial, sans-serif;line-height:20px;-webkit-text-size-adjust:none;text-decoration:underline;"
                                        target="_blank">About Project</a>
                                 </td>
@@ -96,7 +95,7 @@
                                 <td width="29px" style="width:29px;padding-top:10px;">
                                     <a href="https://facebook.com/"
                                        style="color:#194678;font: 12px Arial, sans-serif;line-height:16px;-webkit-text-size-adjust:none;"
-                                       target="_blank"><img src="http://kaddish-prayer.com/kpw/img/fb_email.gif"
+                                       target="_blank"><img src="{{asset("images/mail/fb_email.gif")}}"
                                                             width="29" height="29" border="0"
                                                             style="display:block"/></a>
                                 </td>
@@ -105,15 +104,15 @@
                         </table>
                     </td>
                 </tr>
-                <tr>
-                    <td style="padding-top:10px;border-top:1px solid #c6c6c6;">
-			<span style="color:#777777;font: 12px Arial, sans-serif;">Если желаете больше не получать напоминаний,
-				<a href="qwe"
-                   style="color:#194678;line-height:20px;-webkit-text-size-adjust:none;text-decoration:underline;"
-                   target="_blank">перейдите по этой ссылке</a>.
-			</span>
-                    </td>
-                </tr>
+                {{--                <tr>--}}
+                {{--                    <td style="padding-top:10px;border-top:1px solid #c6c6c6;">--}}
+                {{--			<span style="color:#777777;font: 12px Arial, sans-serif;">Если желаете больше не получать напоминаний,--}}
+                {{--				<a href="qwe"--}}
+                {{--                   style="color:#194678;line-height:20px;-webkit-text-size-adjust:none;text-decoration:underline;"--}}
+                {{--                   target="_blank">перейдите по этой ссылке</a>.--}}
+                {{--			</span>--}}
+                {{--                    </td>--}}
+                {{--                </tr>--}}
                 </tbody>
             </table>
         </td>
